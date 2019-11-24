35f867ae31cd33d8485e618bd6e9e6548a76bd1e
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-11-24 19:28:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-function shout(string) {
-  return string.toUpperCase()
+var uppercase, lowercase
+ 
+function shout(string){
+  uppercase = string.toUpperCase();
+  string = uppercase;
+  return uppercase;
 }
-
 function whisper(string){
-  return string.toLowerCase()
+  lowercase = string.toLowerCase();
+  string = lowercase;
+  return lowercase;
 }
-function shout(){
-  console.log()
+function logShout(string){
+  uppercase = string.toUpperCase()
+  string = uppercase;
+  console.log(uppercase);
 }
+function logWhisper(string){
+  lowercase = string.toLowerCase()
+  string = uppercase;
+  console.log(lowercase);
+}
+function sayHiToGrandma(string){
+    if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }

a3cad15032039068f7a761e9fb7ef7a81a431982
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-11-24 19:46:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ return string.toUpperCase();
 function whisper(string){
 return string.toLowerCase();
 }
-
+function logShout(string){
+uppercase=string.toUpperCase();
+string=uppercase;
+console.log (uppercase);
+}
